0c1ce2d80586d31a1d6b5132fb04d64b168e226e
Jalaj <Jalaj@mail.com>
2021-04-11 10:45:58 +0530

    finish

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-var translateBtn = document.querySelector("#translate-btn");
+const translateBtn = document.querySelector("#translate-btn");
 
-var input = document.querySelector("textarea");
+const input = document.querySelector("textarea");
 
-var divOutput = document.querySelector("#output");
+const divOutput = document.querySelector("#output");
 
-var url = "https://api.funtranslations.com/translate/minion.json";
+const url = "https://api.funtranslations.com/translate/minion.json";
 
-function constructURL(text) {
-  // console.log(encodeURI(`${url}?text=${text}`));
-  return `${url}?text=${text}`;
-}
+const constructURL = (text) => {
+    return `${url}?text=${text}`;
+};
 
-function translateHandler() {
-  var text = input.value;
+const translateHandler = () => {
+    const text = input.value;
 
-  fetch(constructURL(text))
-    .then((response) => response.json())
-    .then((translatedInput) => {
-      console.log(translatedInput);
-      divOutput.innerText = translatedInput.contents.translated;
-    })
-    .catch((error) => {
-      console.log(error);
-      alert("Please try again after some time");
-    });
-}
+    fetch(constructURL(text))
+        .then((response) => response.json())
+        .then((translatedInput) => {
+            console.log(translatedInput);
+            divOutput.innerText = translatedInput.contents.translated;
+        })
+        .catch((error) => {
+            console.log(error);
+            alert("Please try again after some time");
+        });
+};
 
 translateBtn.addEventListener("click", translateHandler);
